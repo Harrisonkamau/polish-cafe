cdaf4b5f31f32e9ca68b74a2dd96fe486ecc5018
Harrisonkamau <kamauharrison87@gmail.com>
2016-06-24 00:00:53 +0300

    validate tabs

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,44 +3,42 @@ $(document).ready(function()
       $('.carousel').carousel({
         interval: 3000
       });
-  $('#contact_form').on('submit', function(e){
+    	$( "#tabs" ).tabs();
+    	$("#resizable" ).resizable();
+      $('#contact_form').on('submit', function(e){
 
-  		//place event handling logic here
-  		 url = '/';
-  		 data = $(this).serialize();
+      		//place event handling logic here
+      		 url = '/';
+      		 data = $(this).serialize();
+      		$.post(url, data);
+      		//prevents the default behvior of the form
+      		e.preventDefault();
+      	});
 
-  		$.post(url, data);
-  		// alert(Still here?);
-
-
-  		//prevents the default behvior of the form
-  		e.preventDefault();
-  	});
-
-  		// validate your form user inputs
-  		$('#contact_form').validate(
-  		{
-  			rules:
-  			{
-  				name:"required",
-  				email: {
-  					required: true,
-  					email: true,
-  					// maxlength: 30
-  				}
-  			},
-  			messages:
-  			{
-  				name: "Please Enter Your Full Names",
-  				email: {
-  					required: "Email cannot be Empty!!",
-  					email: "Please use this format user@somedomain.com",
-  					// maxlength: "Maximum length should be 30"
-  				}
-  			},
-  			highlight: function(element)
-  				{
-  					$(element).addClass('error');
-  				}
-  		});
+      		// validate your form user inputs
+      		$('#contact_form').validate(
+      		{
+      			rules:
+      			{
+      				name:"required",
+      				email: {
+      					required: true,
+      					email: true,
+      					// maxlength: 30
+      				}
+      			},
+      			messages:
+      			{
+      				name: "Please Enter Your Full Names",
+      				email: {
+      					required: "Email cannot be Empty!!",
+      					email: "Please use this format user@somedomain.com",
+      					// maxlength: "Maximum length should be 30"
+      				}
+      			},
+      			highlight: function(element)
+      				{
+      					$(element).addClass('error');
+      				}
+      		});
     });
