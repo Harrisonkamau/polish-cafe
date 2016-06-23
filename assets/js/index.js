70e88c8ddb5f41ca3f6d8add0419efbf1a02ff2f
Harrisonkamau <kamauharrison87@gmail.com>
2016-06-23 23:48:26 +0300

    implement user input validation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,46 @@
 $(document).ready(function()
     {
       $('.carousel').carousel({
-  interval: 3000
-})
+        interval: 3000
+      });
+  $('#contact_form').on('submit', function(e){
+
+  		//place event handling logic here
+  		 url = '/';
+  		 data = $(this).serialize();
+
+  		$.post(url, data);
+  		// alert(Still here?);
+
+
+  		//prevents the default behvior of the form
+  		e.preventDefault();
+  	});
+
+  		// validate your form user inputs
+  		$('#contact_form').validate(
+  		{
+  			rules:
+  			{
+  				name:"required",
+  				email: {
+  					required: true,
+  					email: true,
+  					// maxlength: 30
+  				}
+  			},
+  			messages:
+  			{
+  				name: "Please Enter Your Full Names",
+  				email: {
+  					required: "Email cannot be Empty!!",
+  					email: "Please use this format user@somedomain.com",
+  					// maxlength: "Maximum length should be 30"
+  				}
+  			},
+  			highlight: function(element)
+  				{
+  					$(element).addClass('error');
+  				}
+  		});
     });
