de0ee9e9ffa16f1baa8a6a3c17d749f4ac667f1b
Harrisonkamau <kamauharrison87@gmail.com>
2016-06-24 09:18:12 +0300

    resize tabs

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,42 +3,7 @@ $(document).ready(function()
       $('.carousel').carousel({
         interval: 3000
       });
-    	$( "#tabs" ).tabs();
-    	$("#resizable" ).resizable();
-      $('#contact_form').on('submit', function(e){
 
-      		//place event handling logic here
-      		 url = '/';
-      		 data = $(this).serialize();
-      		$.post(url, data);
-      		//prevents the default behvior of the form
-      		e.preventDefault();
-      	});
-
-      		// validate your form user inputs
-      		$('#contact_form').validate(
-      		{
-      			rules:
-      			{
-      				name:"required",
-      				email: {
-      					required: true,
-      					email: true,
-      					// maxlength: 30
-      				}
-      			},
-      			messages:
-      			{
-      				name: "Please Enter Your Full Names",
-      				email: {
-      					required: "Email cannot be Empty!!",
-      					email: "Please use this format user@somedomain.com",
-      					// maxlength: "Maximum length should be 30"
-      				}
-      			},
-      			highlight: function(element)
-      				{
-      					$(element).addClass('error');
-      				}
-      		});
+    	$( "#tabs").tabs();
+        // alert('debugging');
     });
